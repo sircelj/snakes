fbde0c540660a953c36deb7931f5ac50c4ece198
Jaka Šircelj <sircelj.jaka@gmail.com>
2014-12-04 01:19:17 +0100

    Zbrisal testni kometar, vse deluje! :D

--- game.py
+++ game.py
@@ -17,8 +17,6 @@ from kingCobra import KingCobra
 
 # Nato v spisek SNAKES dodate razred, ki predstavlja vaso kaco
 
-# Dodal sem nov komentar, ki ga bom hitro zbrisal
-
 SNAKES = [
     BolivianAnaconda,
     CongoWaterCobra,
